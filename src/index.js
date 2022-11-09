078fe32e797897209b3b8db7032821270f59efba
Prachi Shrestha <prachi.stha1999@gmail.com>
2022-11-09 15:39:45 +0545

    'update'

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='https://prachi-shrestha.github.io/ensite/'>
+    <HashRouter basename='https://prachi-shrestha.github.io/ensite/'>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
